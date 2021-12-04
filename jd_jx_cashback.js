0fd836a78e1b83a5d9a3f96c2e0b92ae072cf125
mysuperdoor <32043900+mysuperdoor@users.noreply.github.com>
2021-12-04 16:59:37 +0800

    Update jd_jx_cashback.js

--- jd_jx_cashback.js
+++ jd_jx_cashback.js
@@ -1,11 +1,11 @@
 /*
 cron "44 0-23/6 * * *" jx_cashback.js
-new Env('京喜购物返红包助力');
 京喜购物返红包助力
 更新时间：2021-9-10
 备注：京喜购物返红包助力，多个请看下方变量填写
 ## exprot jd_jx_cashback=10  #如需增加被助力账号,在这边修改人数
 */
+const $ = new Env('京喜购物返红包助力');
 let common = require("./function/common");
 let $ = new common.env('京喜购物返红包助力');
 let min = 5,
